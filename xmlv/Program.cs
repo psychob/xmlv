d6b4c3c8c37bc52aab3bcd73aa5bff1badd6844c
Andrzej Budzanowski <psychob.pl@gmail.com>
2016-12-12 11:11:24 +0100

    version 1.0

--- xmlv/Program.cs
+++ xmlv/Program.cs
@@ -1,8 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//
+// xmlv
+// (c) 2016 Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
+using System;
+using System.Xml;
+using System.Xml.Schema;
 
 namespace xmlv
 {
@@ -10,6 +17,43 @@ namespace xmlv
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("xmlv");
+			Console.WriteLine("(c) 2016 Andrzej Budzanowski <kontakt@andrzej.budzanowski.pl>");
+			Console.WriteLine("--");
+
+			XmlSchemaSet xss = new XmlSchemaSet();
+
+			foreach (var it in args)
+			{
+				if (it.EndsWith(".xsd"))
+					xss.Add(null, it);
+			}
+
+			foreach (var it in args)
+			{
+				if (it.EndsWith(".xml"))
+				{
+					XmlReaderSettings set = new XmlReaderSettings();
+					set.ValidationType = ValidationType.Schema;
+					set.Schemas = xss;
+					set.ValidationEventHandler += OnXmlError;
+
+					Console.WriteLine("Validate: {0}", it);
+					CurrentFileName = it;
+
+					using (XmlReader xr = XmlReader.Create(it, set))
+						while (xr.Read()) ;
+				}
+			}
+		}
+
+		private static string CurrentFileName = "";
+
+		private static void OnXmlError(object sender, ValidationEventArgs e)
+		{
+			Console.WriteLine("{0}:{1}:{2} - {3}: {4}", CurrentFileName,
+				e.Exception.LineNumber, e.Exception.LinePosition, e.Severity,
+				e.Message);
 		}
 	}
 }
--- xmlv/Properties/AssemblyInfo.cs
+++ xmlv/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("xmlv")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("simple xml validator utility")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("xmlv")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Andrzej Budzanowski 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
